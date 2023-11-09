2e99452b50084dcf112d68a9c6e0d90de8094334
BrassyThermal <muhammadyusrildevj@gmail.com>
2023-11-09 20:46:15 +0700

    revise hello world feature

--- src/Infrastructures/http/CreateServer.ts
+++ src/Infrastructures/http/CreateServer.ts
@@ -76,31 +76,9 @@ export const CreateServer = async (
   server.route({
     method: "GET",
     path: "/",
-    handler: (request, h) => {
-      const htmlResponse = `<html>
-          <head>
-            <title>Forum Garuda API</title>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          </head>
-          <body style="
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: linear-gradient(to bottom, blue, white);"
-          >
-            <h1 style="
-              font-family: sans-serif;
-              font-size: calc(8vw + 5px);
-              font-weight: 400;
-              color: #E91E63;"
-            >
-              Hello World!
-            </h1>
-          </body>
-        </html>`;
-      return h.response(htmlResponse).type("text/html").code(200);
-    },
+    handler: () => ({
+      value: "Hello World!"
+    })
   });
 
   server.ext("onPreResponse", (request, h) => {
